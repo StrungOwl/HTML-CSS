ea16a348ad45eb95065a754e051eb73867700dfb
StrungOwl <sydneymaeparks@gmail.com>
2024-06-28 15:37:45 -0500

    File Upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // Add your setup code here
+  createCanvas(400, 400); // Example: create a canvas with width and height of 400 pixels
+}
+
+function draw() {
+  // Add your drawing code here
+  background(220); // Example: set the background color to 
+}
